b830ddcf6af95e4ab2bbdafc01fd925494e1c53b
Nguyễn Anh Dũng <dungna29@fpt.edu.vn>
2021-05-14 14:06:23 +0700

    1

---
+++ 6_WE17103_NET101/BAI_1.1_LamQuenCsharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace BAI_1._1_LamQuenCsharp
+{
+    class Program
+    {
+        /*
+         * BÀI 1 LÀM QUEN VỚI VISUAL STUDIO VÀ CÁC PHÍM TẮT CƠ BẢN
+         */
+        /*
+         * Một số phím tắt hay dùng trong VisualStudio 
+         * 1. Chạy chương trình trong VisualStudio phím: F5 hoặc Fn + F5 hoặc Ctrl + F5
+         * 2. Ctrl + D: Dùng duplicate đoạn code
+         * 3. Ctrl + K + C: Dùng để comment code
+         * 4. Ctrl + K + U: Dùng để mở comment code
+         * 5. Ctrl + K + D: Format code giúp clean code
+         * 6. Ctrl + Space: Gợi ý code
+         * Comment code: // hoặc /*
+         */
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding("UTF-8");
+            Console.WriteLine("Hello Các bạn FPOLY UDPM - WEB!");
+            //cw + tab = In ra màn hình
+            Console.WriteLine("Chào các bạn đến với môn C#1"); 
+            Console.WriteLine();
+        }
+    }
+}
